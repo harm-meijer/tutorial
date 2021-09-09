eaa4b486c9b33ce7e5d93574ed283ebfab380cdf
Harm Meijer <harm.meijer@commercetools.com>
2021-09-09 11:17:55 +0200

    step 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from "react";
-import Counter from "./components/Counter";
 
 function App() {
-  const [counters, setCounters] = React.useState(() =>
-    [...new Array(10)].map((_, i) => ({
-      id: i + 1,
-      count: 1,
-    }))
+  const [counter, setCounter] = React.useState(1);
+  //@todo: fix this function
+  const up = React.useCallback(
+    () => setCounter(counter + 1),
+    []
   );
-  //@todo: create up function that will increase a counter by id
-  const up = (id) => "implement this";
   return (
-    <ul>
-      {counters.map((counter) => (
-        <Counter
-          key={counter.id}
-          up={up}
-          counter={counter}
-        />
-      ))}
-    </ul>
+    <>
+      counter:{counter}
+      <button onClick={up}>UP</button>
+    </>
   );
 }
 
