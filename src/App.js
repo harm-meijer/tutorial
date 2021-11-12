68cb92cd5b22feb8a74a0e2477f0daf90d2b109a
Felipe Lemos <felipe.lemos@commercetools.com>
2021-11-12 12:58:31 +0100

    Solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Counter from "./components/Counter";
 
 function App() {
   const [counters, setCounters] = React.useState(() =>
-    [...new Array(10)].map((_, i) => ({
+    [...new Array(10000)].map((_, i) => ({
       id: i + 1,
       count: 1,
     }))
   );
   //@todo: create up function that will increase a counter by id
-  const up = (id) => "implement this";
+  const up = useCallback(
+    (id) =>
+      setCounters((counters) =>
+        counters.map((counter) =>
+          counter.id === id ? { ...counter, count: counter.count + 1 } : counter
+        )
+      ),
+    []
+  );
   return (
     <ul>
       {counters.map((counter) => (
-        <Counter
-          key={counter.id}
-          up={up}
-          counter={counter}
-        />
+        <Counter key={counter.id} up={up} counter={counter} />
       ))}
     </ul>
   );
