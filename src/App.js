dbbcb4e80731b7eb91d7ea7f2af9c2f79b755a21
Felipe Lemos <felipe.lemos@commercetools.com>
2021-11-22 11:40:28 +0100

    Co-authored-by: Harm Meijer <amsterdamharu@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import Item from "./components/Item";
+import { useSelector } from "react-redux";
+import { createSelectItemById } from "./selectors";
 
 function App() {
-  //@todo: select root items and display tree
-  return <div></div>;
+  const rootItem = useSelector(createSelectItemById("root"));
+  return (
+    <div>
+      <ul>
+        {rootItem.children.map((itemId) => (
+          <Item key={itemId} id={itemId} parentId={"root"} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,21 +1,95 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const initialState = {
-  items: [
-    { id: 1, value: "root one" },
-    { id: 2, value: "root two" },
-    { id: 3, value: "Child of one", parent: 1 },
-  ],
+  items: {
+    root: { id: "root", children: [1, 2] },
+    1: { id: 1, children: [3] },
+    2: { id: 2, children: [] },
+    3: { id: 3, children: [] },
+  },
+};
+
+const createId = (items) =>
+  Math.max(
+    ...Object.keys(items)
+      .map((key) => Number(key))
+      .filter((id) => !isNaN(id))
+  ) + 1;
+
+const removeItems = (itemId, items) => {
+  const idsToRemove = [itemId].concat(getDecedents(itemId, items));
+  console.log(idsToRemove);
+  return Object.fromEntries(
+    Object.entries(items).filter(([key]) => !idsToRemove.includes(Number(key)))
+  );
+};
+
+export const getDecedents = (id, items) => {
+  const recur = (acc, id) => {
+    acc.push(...items[id].children);
+    items[id].children.forEach((childId) => {
+      recur(acc, childId);
+    });
+    return acc;
+  };
+  return recur([], id);
 };
 
 const store = createStore(
-  (state = initialState, { type }) => {
+  (state = initialState, { type, payload }) => {
     //@todo: implement adding or removing a child to
     //  tree
-    return { ...state };
+    if (type === "ADD") {
+      const parentId = payload;
+      const id = createId(state.items);
+      return {
+        ...state,
+        items: {
+          ...state.items,
+          [parentId]: {
+            ...state.items[parentId],
+            children: state.items[parentId].children.concat(id),
+          },
+          [id]: {
+            id,
+            children: [],
+          },
+        },
+      };
+    }
+    if (type === "REMOVE") {
+      const { itemId, parentId } = payload;
+      console.log(`item to be removed: ${itemId}, parent id: ${parentId}`);
+      const items = {
+        ...removeItems(itemId, state.items),
+        [parentId]: {
+          ...state.items[parentId],
+          children: state.items[parentId].children.filter(
+            (id) => id !== itemId
+          ),
+        },
+      };
+      return {
+        ...state,
+        items,
+      };
+    }
+
+    return state;
   },
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(({ getState, dispatch }) => (next) => (action) => {
+      if (!["loading", "loaded"].includes(action.type)) {
+        dispatch({ type: "loading" });
+        //do async stuff
+        Promise.resolve().then(() =>
+          dispatch({ type: "loaded", payload: "some data" })
+        );
+      }
+      return next(action);
+    })
+  )
 );
 
 export default store;
